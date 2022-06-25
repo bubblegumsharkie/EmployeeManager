ae6fcb23db7df873084c43eaae0bf39a31925a75
Alexey Kozlov <kozlov.alex.dev@gmail.com>
2022-06-25 10:59:55 +0300

    Service and Repo ✅

--- src/main/java/com/studyhardpro/EmployeeManager/services/EmployeeService.java
+++ src/main/java/com/studyhardpro/EmployeeManager/services/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.studyhardpro.EmployeeManager.services;
 
+import com.studyhardpro.EmployeeManager.exceptions.UserNotFoundException;
 import com.studyhardpro.EmployeeManager.models.Employee;
 import com.studyhardpro.EmployeeManager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,6 +21,23 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("The user with the id: " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
     }
 
 }
